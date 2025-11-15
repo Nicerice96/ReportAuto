3b83b497818ba27a1d9a58d8dffce80523e926c3
Nicerice96 <100174834+Nicerice96@users.noreply.github.com>
2025-11-15 10:02:30 -0500

    adding API secret

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # This is a automation script to automatically send information from Depenedncy Track (localhost:8080) to email.
-API_KEY = ""
+import os
+API_KEY = os.getenv("API_KEY_DEPENDENCY_TRACK")
+
 
 
 
